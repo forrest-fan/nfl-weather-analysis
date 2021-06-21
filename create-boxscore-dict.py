af50d994e74164ec9dd11c0c181cd5b94df35ea5
Forrest Fan <forrest.c.fan@gmail.com>
2021-06-21 12:13:59 -0400

    create dict that maps game ID to array of players stat lines in that game

---
+++ create-boxscore-dict.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json
+
+df = pd.read_pickle("all-fantasy-stats.pkl")
+
+boxDict = {}
+
+for i, row in df.iterrows():
+    gameID = row["Game ID"]
+    if gameID in boxDict:
+        boxDict[gameID].append(row.to_dict())
+    else:
+        boxDict[gameID] = [row.to_dict()]
+
+with open('boxscore-dict.json', 'w+') as f:
+    json.dump(boxDict, f)
+
+print("done")
